586d26f10d54878ef528e9f9435f337f727991f1
Joe Manthey <jdmanthey@gmail.com>
2015-10-08 11:44:01 -0500

    Create convert_bedassle.r

---
+++ convert_bedassle.r
@@ -0,0 +1,112 @@
+convert_bedassle <- function(structure, popmap) {
+	library(BEDASSLE)
+	
+	x <- read.table(structure, sep = "\t")
+	pops <- read.table(popmap, sep = "\t")
+	inds <- as.character(x[2:nrow(x), 1])
+	n.pops <- length(unique(pops[,2]))
+	
+	# remove the population identification column or not
+	if (is.na(x[1, 2]) == TRUE) {
+		x <- cbind(x[,1], x[,3:ncol(x)])
+	}
+	
+	snp.names <- as.vector(as.character(x[1,2:ncol(x)]))
+	
+	
+	#######################################################################
+	#
+	# determine that a SNP is in each population, remove SNPs that are not
+	for (a in 1:ncol(x)) {
+		if(a == 1) {
+			x2 <- as.character(x[,1])
+		} else {
+			x.test <- x[2:nrow(x),a]
+			skip <- 0
+			for(b in 1:n.pops) {
+				test <- match(inds,as.character(pops[pops[,2]==b,1]))
+				test[is.na(test)] <- 0
+				test <- x.test[test > 0]
+				test <- test[test > 0]
+				if(length(test) == 0) {
+					skip <- skip + 1
+				}
+			}
+			if(skip == 0) {
+				x.test <- c(snp.names[a-1], x.test)
+				x2 <- cbind(x2,x.test)
+			}
+		}
+	}
+	# remove items 
+	x <- x2
+	rm(a,b,skip,test,x.test,x2,popmap,structure)
+	snp.names <- x[1,2:ncol(x)]
+	snp.names <- as.vector(as.character(snp.names))
+	
+	
+	######################################################################
+	#
+	# determine if locus is biallelic, remove if not
+	for(a in 1:ncol(x)) {
+		if(a == 1) {
+			x2 <- x[,1]
+		} else {
+			x.test <- x[2:nrow(x),a]
+			x.test <- x.test[x.test > 0]
+			if(length(unique(x.test))==2) {
+				x.test <- c(snp.names[a-1], x[2:nrow(x),a])
+				x2 <- cbind(x2, x.test)
+			}
+		}
+	}
+	# remove items
+	x <- x2
+	rm(x2, a, x.test)
+	snp.names <- x[1,2:ncol(x)]
+	snp.names <- as.vector(as.character(snp.names))
+	
+	
+	######################################################################
+	#
+	# create allele frequency and sample size matrices
+	for(a in 1:ncol(x)) {
+		if(a == 1) {
+		} else {
+			x.rep <- as.vector(x[2:nrow(x),a])
+			allele.a <- x.rep[x.rep > 0]
+			allele.a <- allele.a[1]
+			alleles <- c()
+			samples <- c()
+			for(b in 1:n.pops) {
+				test <- match(inds,as.character(pops[pops[,2]==b,1]))
+				test[is.na(test)] <- 0
+				test <- x.rep[test > 0]
+				alleles <- c(alleles, length(na.omit(match(test, allele.a))))
+				samples <- c(samples, length(test[test>0]))
+			}
+			if(a == 2) {
+				allele.matrix <- alleles
+				sample.size.matrix <- samples
+			} else {
+				allele.matrix <- cbind(allele.matrix, alleles)
+				sample.size.matrix <- cbind(sample.size.matrix, samples)
+			}
+		}
+	}
+	# remove items and give row/column names
+	snp.names <- c(snp.names)
+	colnames(allele.matrix) <- snp.names
+	rownames(allele.matrix) <- 1:n.pops
+	colnames(sample.size.matrix) <- snp.names
+	rownames(sample.size.matrix) <- 1:n.pops
+	
+	write.table(allele.matrix, file="bedassle.allele.counts.txt", sep="\t",
+		quote=FALSE, row.names=FALSE, col.names=FALSE)	
+	write.table(sample.size.matrix, file="bedassle.sample.sizes.txt", sep="\t",
+		quote=FALSE, row.names=FALSE, col.names=FALSE)		
+	
+	
+	
+}
+
